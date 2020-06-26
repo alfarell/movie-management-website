98d2e2c819477e1059b1f677f32784a4bcf4e984
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-06-26 12:15:41 +0700

    [UPDATE] filter favorited movie, when movie is already added to favorite movie and favorite button clicked again it will delete the movie from favorited movie

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -5,18 +5,19 @@ import CardContent from '../components/CardContent';
 const FavoritesPage = () => {
     const favoritedMovie = JSON.parse(localStorage.getItem('favorited'));
 
+    console.log(favoritedMovie)
     return (
         <Card title='Favorite Movie'>
             <Row>
-                {favoritedMovie.length === 0
-                    ? null
-                    : favoritedMovie.map(movie => {
+                {favoritedMovie
+                    ? favoritedMovie.map(movie => {
                         return (
                             <Col key={movie.id} xs={11} sm={7} md={6} lg={6} xl={4} xxl={3} >
                                 <CardContent data={movie} />
                             </Col>
                         )
                     })
+                    : null
                 }
             </Row>
         </Card>
--- src/services/AppContextProvider.js
+++ src/services/AppContextProvider.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
+import _ from 'loadsh';
 import Axios from 'axios';
 
 
@@ -58,13 +59,22 @@ const AppContextProvider = ({ children }) => {
             });
     }
 
-    const addFavoriteMovie = (movie) => {
-        const currentMovie = JSON.parse(localStorage.getItem('favorited'));
+    const addFavoriteMovie = (favoritedMovie) => {
+        let currentMovie = JSON.parse(localStorage.getItem('favorited'));
         let addFavorited;
 
-        currentMovie
-            ? addFavorited = [...currentMovie, movie]
-            : addFavorited = [movie];
+        if (currentMovie) {
+            let check = _.find(currentMovie, favoritedMovie);
+            if (check) {
+                addFavorited = currentMovie.filter(movie => {
+                    return movie.id !== favoritedMovie.id
+                });
+            } else {
+                addFavorited = [...currentMovie, favoritedMovie]
+            }
+        } else {
+            addFavorited = [favoritedMovie];
+        }
 
         localStorage.setItem('favorited', JSON.stringify(addFavorited));
     }
