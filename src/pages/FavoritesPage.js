6f1c1bf5dd6177e64d8d1b3ecaec95e51692fd48
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-06-26 22:03:19 +0700

    [ADD] add empty icon when favorite movie list is empty

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Col, Empty } from 'antd';
 import CardContent from '../components/CardContent';
 
 const FavoritesPage = () => {
@@ -8,15 +8,15 @@ const FavoritesPage = () => {
     return (
         <Card title='Favorite Movie'>
             <Row gutter={[5, 5]} justify='center'>
-                {favoritedMovie
-                    ? favoritedMovie.map(movie => {
+                {favoritedMovie.length === 0
+                    ? <Empty description={'No Favorite Movie'} />
+                    : favoritedMovie.map(movie => {
                         return (
                             <Col key={movie.id} xs={11} sm={7} md={6} lg={6} xl={4} xxl={3} >
                                 <CardContent data={movie} />
                             </Col>
                         )
                     })
-                    : null
                 }
             </Row>
         </Card>
