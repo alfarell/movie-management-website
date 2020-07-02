f00fe72e2d40efb0a0db9749de01c4b6dc786d69
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-07-02 19:38:10 +0700

    [UPDATE] change style and add hide mode to set the favorite button is automatically hide the label or always show the label

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import { Button } from 'antd';
 import { HeartFilled } from '@ant-design/icons';
 
-const FavoriteButton = ({ label, type, onClick, style, iconStyle }) => {
+const FavoriteButton = ({ label, type, hideMode, onClick, style, iconStyle }) => {
     const [showLabel, setShowLabel] = useState(false);
 
     return (
         <Button
             danger
-            id='favorite-button'
             type={type}
             icon={<HeartFilled style={iconStyle} />}
-            style={{ ...style, position: 'absolute', zIndex: 1 }}
+            style={{ ...style, zIndex: 1 }}
             onMouseEnter={() => setShowLabel(true)}
             onMouseLeave={() => setShowLabel(false)}
             onClick={onClick}
         >
-            {showLabel ? label : null}
+            {hideMode
+                ? showLabel ? label : null
+                : label
+            }
         </Button>
     );
 };
