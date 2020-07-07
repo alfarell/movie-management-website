d0dc3da29418814abc5c9aa1c5dc5193f8f32c8d
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-07-07 14:38:26 +0700

    [UPDATE] add unit test of favorite page when movie list is not null

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -9,7 +9,7 @@ const FavoritesPage = () => {
     return (
         <div style={{ padding: '10px 20px' }}>
             <Card title='Favorite Movie' style={{ minHeight: '75vh' }}>
-                <Row gutter={[5, 5]} justify='center'>
+                <Row gutter={[5, 5]} justify='center' data-testid='favorite-list'>
                     {listFavoriteMovie.length === 0
                         ? <Empty description={'No Favorite Movie'} />
                         : listFavoriteMovie.map(movie => {
