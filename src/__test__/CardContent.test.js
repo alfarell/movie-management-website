4c10a460d43630171d8662a14bf8433c7618f705
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-07-07 15:19:25 +0700

    [ADD] unit test of Card Content component

---
+++ src/__test__/CardContent.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CardContent from '../components/CardContent';
+import AppContextProvider from '../services/AppContextProvider';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const dummyData = {
+    id: 1,
+    title: 'test',
+    poster_path: 'test_poster',
+    vote_average: 9
+}
+
+test('should show favorite button when mouse enter the display movie card', () => {
+    const component = renderer.create(
+        <AppContextProvider>
+            <Router>
+                <CardContent data={dummyData} />
+            </Router>
+        </AppContextProvider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    renderer.act(() => {
+        tree.props.onMouseEnter();
+    })
+
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('should show favorite button when mouse leave the display movie card', () => {
+    const component = renderer.create(
+        <AppContextProvider>
+            <Router>
+                <CardContent data={dummyData} />
+            </Router>
+        </AppContextProvider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    renderer.act(() => {
+        tree.props.onMouseLeave();
+    })
+
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
