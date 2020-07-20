ef5850bb08bc93bf6ed48a657b10a81b2f39f141
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-07-20 10:18:39 +0700

    [UPDATE] change default value of selected genre to empty string, selected genre tag code style, add selected menu item key and default open sub menu key

--- src/components/MainComponent/DisplayFilters.js
+++ src/components/MainComponent/DisplayFilters.js
@@ -3,21 +3,45 @@ import { Menu, Tag } from 'antd';
 import { MovieContext } from '../../services/AppContextProvider';
 
 const DisplayFilters = () => {
-    const { genreList, selectedGenre, setSelectedGenre, setSortOption } = useContext(MovieContext);
+    const { genreList, selectedGenre, sortOption, setSelectedGenre, setSortOption } = useContext(MovieContext);
 
     return (
-        <Menu mode='inline'>
+        <Menu
+            mode='inline'
+            selectable
+            selectedKeys={[selectedGenre?.id?.toString(), sortOption]}
+            defaultOpenKeys={['sort-option']}
+        >
             <div>
-                {
-                    selectedGenre
-                        ? <Tag color='blue' closable onClose={() => setSelectedGenre({})} data-testid='selected-genre'>{selectedGenre.name}</Tag>
-                        : null
-                }
+                {selectedGenre && (
+                    <Tag
+                        color='blue'
+                        closable
+                        onClose={() => setSelectedGenre('')}
+                        data-testid='selected-genre'>
+                        {selectedGenre.name}
+                    </Tag>
+                )}
             </div>
-            <Menu.SubMenu title='Sort By'>
-                <Menu.Item onClick={() => setSortOption('popularity.desc')}>Popularity</Menu.Item>
-                <Menu.Item onClick={() => setSortOption('release_date.desc')}>Latest Release</Menu.Item>
-                <Menu.Item onClick={() => setSortOption('release_date.asc')}>Oldest Release</Menu.Item>
+            <Menu.SubMenu title='Sort By' key='sort-option'>
+                <Menu.Item
+                    onClick={() => setSortOption('popularity.desc')}
+                    key='popularity.desc'
+                >
+                    Popularity
+                </Menu.Item>
+                <Menu.Item
+                    onClick={() => setSortOption('release_date.desc')}
+                    key='release_date.desc'
+                >
+                    Latest Release
+                </Menu.Item>
+                <Menu.Item
+                    onClick={() => setSortOption('release_date.asc')}
+                    key='release_date.asc'
+                >
+                    Oldest Release
+                </Menu.Item>
             </Menu.SubMenu>
             <Menu.SubMenu title={<span>Select Genre</span>} disabled={genreList.length === 0 ? true : false} >
                 {genreList.map(genre => {
